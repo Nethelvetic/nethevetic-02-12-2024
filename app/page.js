6952a055e313fdddc3543258da342e42576ab5ae
Golliard <golliard.info@gmail.com>
2024-12-09 08:44:55 +0100

    commite troisième

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function HomePage() {
       <br></br>
 
       
-      <CardHImgTitleLg imageSrc="jeroNum181Bg.png" title="Construisez un business qui fonctionne pour vous, pas l’inverse"/>
+      <CardHImgTitleLg imageSrc="jeroNum181Bg.png" title="Construisez un business qui fonctionne pour vous, pas l’inverse 3"/>
 
       <CardHImgTxtLg imageSrc="globe.svg" title="Nethelvetic4" content="lLe Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker. " />
     </div>
